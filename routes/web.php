b0e3cea6805d574324b399620ac3cc773fd4c8ff
linusyen <kenshin1988x@gmail.com>
2020-12-23 14:40:46 +0800

    add routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UrlController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/{key}', [UrlController::class, 'redirect']);
+Route::get('/', [UrlController::class, 'index']);
